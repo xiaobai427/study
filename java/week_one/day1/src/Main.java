c6fb9828df704e9a5ea928aad1270ece430abf82
shibo.huang <2647249388@qq.com>
2023-04-24 22:39:31 +0800

    add function

--- java/week_one/day1/src/Main.java
+++ java/week_one/day1/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+
     public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+
+        int sum = a + b;
+        System.out.println(sum);
         System.out.println("Hello world!");
     }
 }
